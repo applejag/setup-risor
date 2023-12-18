a3349a4ba4f748bc47bf221d8eab30bded3214b0
Kalle Fagerberg <kalle.fagerberg@riskident.com>
2023-12-19 00:17:15 +0100

    npm run build

--- dist/index.js
+++ dist/index.js
@@ -30503,92 +30503,96 @@ const core = __nccwpck_require__(2186);
 const tc = __nccwpck_require__(7784);
 
 // Used as fallback if getting latest version fails.
-const latestKnownVersion = "v1.2.0";
+const latestKnownVersion = 'v1.2.0';
 
 async function run() {
-	const cache = core.getBooleanInput('cache');
-	var version = core.getInput("version", { required: true });
+  const cache = core.getBooleanInput('cache');
+  var version = core.getInput('version', { required: true });
 
-	if (version === 'latest') {
-		version = await findLatestVersion();
-	}
+  if (version === 'latest') {
+    version = await findLatestVersion();
+  }
 
-	if (cache) {
-		const risorDir = tc.find('risor', version);
-		if (risorDir) {
-			core.info(`Found Risor@${version} in cache: ${risorDir}`);
-			core.addPath(risorDir);
-			return;
-		}
-	}
+  if (cache) {
+    const risorDir = tc.find('risor', version);
+    if (risorDir) {
+      core.info(`Found Risor@${version} in cache: ${risorDir}`);
+      core.addPath(risorDir);
+      return;
+    }
+  }
 
-	const downloadUrl = getDownloadUrl(version);
-	core.info(`Downloading Risor ${version} from ${downloadUrl}`);
-	const downloadPath = await tc.downloadTool(downloadUrl);
-	const extractedPath = await extract(downloadPath);
+  const downloadUrl = getDownloadUrl(version);
+  core.info(`Downloading Risor ${version} from ${downloadUrl}`);
+  const downloadPath = await tc.downloadTool(downloadUrl);
+  const extractedPath = await extract(downloadPath);
 
-	if (cache) {
-		const toolPath = await tc.cacheDir(extractedPath, "risor", version);
-		core.addPath(toolPath);
-	} else {
-		core.addPath(extractedPath);
-	}
+  if (cache) {
+    const toolPath = await tc.cacheDir(extractedPath, 'risor', version);
+    core.addPath(toolPath);
+  } else {
+    core.addPath(extractedPath);
+  }
 }
 
 /**
  * @param {string} file
  */
 async function extract(file) {
-	if (file.endsWith('.zip')) {
-		return tc.extractZip(file);
-	}
-	return tc.extractTar(file);
+  if (file.endsWith('.zip')) {
+    return tc.extractZip(file);
+  }
+  return tc.extractTar(file);
 }
 
 /**
  * @returns {Promise<string>}
  */
 async function findLatestVersion() {
-	try {
-		const httpClient = new http.HttpClient();
-		const response = await httpClient.getJson(`https://github.com/risor-io/risor/releases/latest`)
-		const version = response.result.tag_name;
-		core.info(`Found latest version of Risor: ${version}`);
-		return version;
-	} catch (err) {
-		core.warning(`Error while fetching latest Risor release: ${err.toString()}`);
-		return latestKnownVersion;
-	}
+  try {
+    const httpClient = new http.HttpClient();
+    const response = await httpClient.getJson(
+      `https://github.com/risor-io/risor/releases/latest`,
+    );
+    const version = response.result.tag_name;
+    core.info(`Found latest version of Risor: ${version}`);
+    return version;
+  } catch (err) {
+    core.warning(
+      `Error while fetching latest Risor release: ${err.toString()}`,
+    );
+    return latestKnownVersion;
+  }
 }
 
-const LINUX = 'Linux'
-const MAC_OS = 'Darwin'
-const WINDOWS = 'Windows_NT'
-const ARM64 = 'arm64'
+const LINUX = 'Linux';
+const MAC_OS = 'Darwin';
+const WINDOWS = 'Windows_NT';
+const ARM64 = 'arm64';
 
 /**
  * @param {string} version
  * @returns {string}
  */
 function getDownloadUrl(version) {
-	const arch = os.arch()
-	const operatingSystem = os.type()
+  const arch = os.arch();
+  const operatingSystem = os.type();
 
-	switch (true) {
-		case operatingSystem == LINUX && arch == ARM64:
-			return `https://github.com/risor-io/risor/releases/download/${version}/risor_Linux_arm64.tar.gz`;
-		case operatingSystem == LINUX:
-			return `https://github.com/risor-io/risor/releases/download/${version}/risor_Linux_x86_64.tar.gz`;
+  switch (true) {
+    case operatingSystem == LINUX && arch == ARM64:
+      return `https://github.com/risor-io/risor/releases/download/${version}/risor_Linux_arm64.tar.gz`;
+    case operatingSystem == LINUX:
+      return `https://github.com/risor-io/risor/releases/download/${version}/risor_Linux_x86_64.tar.gz`;
 
-		case operatingSystem == MAC_OS && arch == ARM64:
-			return `https://github.com/risor-io/risor/releases/download/${version}/risor_Darwin_arm64.tar.gz`;
-		case operatingSystem == MAC_OS:
-			return `https://github.com/risor-io/risor/releases/download/${version}/risor_Darwin_x86_64.tar.gz`;
+    case operatingSystem == MAC_OS && arch == ARM64:
+      return `https://github.com/risor-io/risor/releases/download/${version}/risor_Darwin_arm64.tar.gz`;
+    case operatingSystem == MAC_OS:
+      return `https://github.com/risor-io/risor/releases/download/${version}/risor_Darwin_x86_64.tar.gz`;
 
-		case operatingSystem == WINDOWS:
-		default:
-			return `https://github.com/risor-io/risor/releases/download/${version}/risor_Windows_x86_64.zip`;
-	}
+    case operatingSystem == WINDOWS:
+    default:
+      return `https://github.com/risor-io/risor/releases/download/${version}/risor_Windows_x86_64.zip`;
+  }
 }
 
 run();
