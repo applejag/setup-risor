d57f6007dd91643cc77fc14b3f0ef218d42dc953
Kalle Fagerberg <kalle.fagerberg@riskident.com>
2024-05-01 16:30:04 +0200

    Update to Risor v1.6.0

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const core = require('@actions/core');
 const tc = require('@actions/tool-cache');
 
 // Used as fallback if getting latest version fails.
-const latestKnownVersion = 'v1.5.1';
+const latestKnownVersion = 'v1.6.0';
 
 async function run() {
   const cache = core.getBooleanInput('cache');
